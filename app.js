570121b06fbb851c30331c7867b234536187edc3
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-11-05 17:44:43 -0300

    feat: connect to database

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const app = express();
+const mongoose = require('mongoose');
+
 const port = process.env.PORT || 3000;
 
-const routes = require('./routes');
-app.use('/', routes);
+const user_routes = require('./routes/user_routes');
+
+app.use('/', user_routes);
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect(process.env.DB_URL).then(function(){
+    console.log("Conectado ao banco de dados");
+  }).catch(function(err){
+    console.log("Error to connect to mongodb:" + err);
+  });
 
 app.listen(port, () => {
     console.log(`Servidor rodando na porta ${port}`);
