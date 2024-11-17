f27a3af3d5118658afd570a54186b80b1d1e50b2
NathanAzvdo <nathan.azevedo28@gmail.com>
2024-11-17 09:34:45 -0300

    feat: validação email cadastrado

--- controllers/servercontroller.js
+++ controllers/servercontroller.js
@@ -6,9 +6,19 @@ exports.getServerMessage = (req, res) => {
 
 exports.add_user = async (req, res) => {
     try {
+        if (!req.body.nome || !req.body.email || !req.body.senha) {
+            return res.status(400).json("Todos os campos são obrigatórios!");
+        }
+
+        const existingUser = await Usuario.findOne({ email: req.body.email });
+        if (existingUser) {
+            return res.status(409).json("Email já cadastrado!");
+        }
+
         const usuario = new Usuario(req.body);
         await usuario.save();
         res.status(200).json(usuario);
+        
     } catch (err) {
         res.status(500).json(err.message);
     }
